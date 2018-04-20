f523df37180e11d687c909b56f74211a57f14335
alexloev <alexloev1988@gmail.com>
2018-04-20 17:50:33 +0300

    hw store commit

---
+++ jsx/store/js/ShopItem.js
@@ -0,0 +1,20 @@
+class ShopItem extends React.Component {
+    render() {
+        return (
+            <div className="main-content">
+                <h2>{this.props.item.brand}</h2>
+                <h1>{this.props.item.title}</h1>
+                <h3>{this.props.item.description}</h3>
+                <div className="description">
+                    {this.props.item.descriptionFull}
+                </div>
+                <div className="highlight-window  mobile"><div class="highlight-overlay"></div></div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                    <div className="price">{this.props.item.currency} {Number(this.props.item.price).toFixed(2)}</div>
+                    <button>Добавить в корзину</button>
+                </div>
+            </div>
+        );
+    }
+}
