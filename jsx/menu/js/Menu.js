637352a6e54e65ac6741d214657fc31745c9bb38
alexloev <alexloev1988@gmail.com>
2018-04-23 17:29:01 +0300

    hw menu commit

--- jsx/menu/js/Menu.js
+++ jsx/menu/js/Menu.js
@@ -1,16 +1,29 @@
 class Menu extends React.Component {
     render() {
-        return (
-            <div class="menu menu-open">
-                <div class="menu-toggle"><span></span></div>
-                <nav>
-                    <ul>
-                        <li><a href="#home">{this.props.items[0].title}</a></li>
-                        <li><a href="#about">{this.props.item[0]}</a></li>
-                        <li><a href="#contact">{this.props.items}</a></li>
-                    </ul>
-                </nav>
-            </div>
-        );
+        if (this.props.opened) {
+            return (
+                <div className="menu menu-open">
+                    <div className="menu-toggle"><span></span></div>
+                    <nav>
+                        <ul>
+                            {this.props.items.map(menuItem)}
+                        </ul>
+                    </nav>
+                </div>
+            );
+        } else {
+            return (
+                <div className="menu menu-toggle">
+                    <div className="menu-toggle"><span></span></div>
+                </div>
+            );
+
+        }
     }
+};
+
+function menuItem(item) {
+    return (
+        <li><a href={item.href}>{item.title}</a></li>
+    );
 }
