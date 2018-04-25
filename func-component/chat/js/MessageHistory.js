95ceb5f94c051606cccc9651c7fa907ede132209
alexloev <alexloev1988@gmail.com>
2018-04-25 16:59:56 +0300

    hw chat commit

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function MessageHistory({ list }) {
+    console.log(list);
+    console.log('sss')
+    list = list.map(item => {
+        var MessageType = Response;
+        
+        switch (item.type) {
+            case "response": MessageType = Response; break;
+            case "message": MessageType = Message; break;
+            case "typing": MessageType = Typing; break;
+        }
+        
+        return (<li key={item.id} className="clearfix"><MessageType from={item.from} message={item} /></li>)
+
+
+    })
+    if (list) {
+        return (
+            <ul>{list}</ul>
+        )
+    } else {
+        return null
+    }
+}
