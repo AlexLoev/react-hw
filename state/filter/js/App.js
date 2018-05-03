cca070c2c43d8da79f7daf9ecf7d120385e023ab
alexloev <alexloev1988@gmail.com>
2018-05-03 22:30:02 +0300

    state filter complete

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,29 @@
 'use strict'
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: 'Websites'
+    }
+  };
+  onChangeFilter(filter) {
+    this.setState({ filter: filter })
+  }
+  filterProjects(props) {
+    return this.props.projects.filter(project =>
+      project.category == this.state.filter || this.state.filter == 'All'
+    )
+  }
+  render() {
+    return (
+      <div>
+        <Toolbar
+          filters={this.props.filters}
+          selected={this.state.filter}
+          onSelectFilter={this.onChangeFilter.bind(this)} />
+        <Portfolio projects={this.filterProjects()} />
+      </div>
+    );
+  }
+}
