38e57400907994ca51328968a7f8d70ab80b6be0
alexloev <alexloev1988@gmail.com>
2018-05-06 21:10:00 +0300

    improve cycles

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,10 +1,10 @@
 'use strict';
 
 function Stars({count}) {
-  let stars = [];
-  for (let i = 0; i < count; i++) {
-    stars.push(<Star />)
-  }
+let stars = Array.from({length: count}).map(() => <Star />);
+  // for (let i = 0; i < count; i++) {
+  //   stars.push(<Star />)
+  // }
   
   return <ul className="card-body-stars u-clearfix"><li>{stars}</li></ul>;
 }
