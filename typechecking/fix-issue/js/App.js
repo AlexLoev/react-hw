3c09b2288ff5f8b5ad14f15d80a42d2a28e5a51b
AlexLoev <alexloev1988@gmail.com>
2018-05-19 22:48:38 +0300

    commit fix-issue

--- typechecking/fix-issue/js/App.js
+++ typechecking/fix-issue/js/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
 
   onChange(e) {
     const target = e.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === 'checkbox' ? target.checked : target.type === 'number' ? Number(target.value) : target.value;
     const name = target.name;
 
     this.setState({
@@ -42,13 +42,16 @@ class App extends React.Component {
   }
 
   render() {
-    const menuItems = ['Google', 'https://google.com', 'Яндекс', 'https://yandex.ru'];
+    const menuItems = [
+      { name: 'Google', url: 'https://google.com' },
+      { name: 'Яндекс', url: 'https://yandex.ru' }
+    ];
 
     return (
       <React.Fragment>
-        <Menu handleSearch={'this.onSearch'} title={'Приложение'} version={'1.3.23'} items={[menuItems]}/>
+        <Menu handleSearch={this.onSearch} title={'Приложение'} version={'1.3'} items={menuItems} />
         <div className="row">
-          <Form {...this.state} handleChange={this.onChange} handleSubmit={this.onSubmit}/>
+          <Form {...this.state} handleChange={this.onChange} handleSubmit={this.onSubmit} />
         </div>
       </React.Fragment>
     );
