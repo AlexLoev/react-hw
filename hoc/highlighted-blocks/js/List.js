f33152af2f9f3e7e17d7188f2f39cf7111ff94e7
AlexLoev <alexloev1988@gmail.com>
2018-06-05 21:54:17 +0300

    commit hoc highlighted-blocks

--- hoc/highlighted-blocks/js/List.js
+++ hoc/highlighted-blocks/js/List.js
@@ -1,16 +1,39 @@
 'use strict';
 
+function viewsWrapper(Component) {
+    return class extends React.Component {
+        render() {
+            // задаем обертку по умолчанию, которая просто выводит дочерний компонент "как есть"
+            var WrapComponent = props => { return props.children }
+            // если количество просмотров следует подсветить, то переопределяем обертку
+            if (this.props.views > 1000) {
+                WrapComponent = Popular
+            } else if (this.props.views < 100) {
+                WrapComponent = New
+            }
+            return (
+                <WrapComponent>
+                    <Component {...this.props} />
+                </WrapComponent>
+            )
+        }
+    }
+}
+
+const WrappedArticle = viewsWrapper(Article);
+const WrappedVideo = viewsWrapper(Video);
+
 const List = props => {
     return props.list.map(item => {
         switch (item.type) {
             case 'video':
                 return (
-                    <Video {...item} />
+                    <WrappedVideo {...item} />
                 );
 
             case 'article':
                 return (
-                    <Article {...item} />
+                    <WrappedArticle {...item} />
                 );
         }
     });
