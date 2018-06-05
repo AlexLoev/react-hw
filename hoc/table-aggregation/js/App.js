c0d78a13278be086c30bb187a839c8cea135f632
AlexLoev <alexloev1988@gmail.com>
2018-06-05 23:39:54 +0300

    commit hoc table-aggregation

--- hoc/table-aggregation/js/App.js
+++ hoc/table-aggregation/js/App.js
@@ -17,10 +17,37 @@ class App extends React.Component {
     render() {
         return (
             <div id="app">
-                <MonthTable list={this.state.list} />
-                <YearTable list={this.state.list} />
-                <SortTable list={this.state.list} />
+                <PrepMonthTable list={this.state.list} />
+                <PrepYearTable list={this.state.list} />
+                <PrepSortTable list={this.state.list} />
             </div>
         );
     }
 };
+
+const PrepMonthTable = PrepareTables(MonthTable);
+const PrepYearTable = PrepareTables(YearTable);
+const PrepSortTable = PrepareTables(SortTable);
+
+function PrepareTables(Component) {
+    return class extends React.Component {
+        render() {
+            console.log("HOC", this.props)
+            if (this.props.list) {
+                this.props.list.map(item => {
+                    const iDate = new Date(item.date);
+                    const locale = "en-us";
+                    item.month = iDate.toLocaleString(locale, { month: "long" });
+                    item.year = iDate.getFullYear();
+                });
+                var grmonths = this.props.list.reduce(function (r, o) {  
+                    var indx = r.findIndex(x => x["month"] === o["month"]);
+                    indx != -1 ? r[indx].amount += o.amount : r.push(o);   
+                    return r;
+                }, []);
+                console.log(grmonths)    
+            }
+            return <Component {...this.props} />
+        }
+    }
+}
