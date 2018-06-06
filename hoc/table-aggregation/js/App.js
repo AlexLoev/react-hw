583ca82478de7491c170f92562707b94cc9f289d
AlexLoev <alexloev1988@gmail.com>
2018-06-06 23:50:53 +0300

    fix sort

--- hoc/table-aggregation/js/App.js
+++ hoc/table-aggregation/js/App.js
@@ -25,33 +25,40 @@ class App extends React.Component {
     }
 };
 
-const PrepMonthTable = PrepareTables(MonthTable, "month");
-const PrepYearTable = PrepareTables(YearTable, "year");
-const PrepSortTable = PrepareTables(SortTable);
+const PrepMonthTable = PrepareTables(MonthTable, "month", "monthNum");
+const PrepYearTable = PrepareTables(YearTable, "year", "year");
+const PrepSortTable = PrepareTables(SortTable, "date", "date");
 
-function PrepareTables(Component, GroupField) {
+function PrepareTables(Component, GroupField, SortField) {
     return class extends React.Component {
         render() {
-            // console.log("HOC", this.props)
-            if (this.props.list) {
+            if (this.props.list.length) {
+                // расширяем массив новыми полями
                 this.props.list.map(item => {
                     const iDate = new Date(item.date);
                     const locale = "en-us";
                     item.month = iDate.toLocaleString(locale, { month: "long" });
+                    item.monthNum = iDate.getMonth();
                     item.year = iDate.getFullYear();
                 });
+                // применяем сортировку, если запросили
+                if (SortField) {
+                    this.props.list.sort(function (a, b) {
+                        var textA = a[SortField];
+                        var textB = b[SortField];
+                        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+                    });
+                }
+                // применяем группировку, если запросили
                 if (GroupField) {
-                    var groupedArray = this.props.list.reduce(function (r, o) {  
+                    this.props.list = this.props.list.reduce(function (r, o) {
                         var indx = r.findIndex(x => x[GroupField] === o[GroupField]);
-                        indx != -1 ? r[indx].amount += o.amount : r.push(o);   
+                        indx != -1 ? r[indx].amount += o.amount : r.push({ ...o });   // пришлось использовать деструктор, чтобы создавались новые объекты, а не ссылки на существующие
                         return r;
                     }, []);
-                } else {
-                    groupedArray = this.props.list;
                 }
-                // console.log("groupedArray",groupedArray)
             }
-            return <Component list = {groupedArray} />
+            return <Component {...this.props} />
         }
     }
 }
